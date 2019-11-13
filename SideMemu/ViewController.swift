594254957e0ae94a2b024f91016f5fa70904bafd
Jian Ma <jianquanma@gmail.com>
2019-11-12 19:10:22 -0500

    added toggle constraint func

--- SideMemu/ViewController.swift
+++ SideMemu/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var viewLeadingContraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+  //      viewLeadingContraint.constant = -200
     }
 
-
+    @IBAction func didTappedMemuButton(_ sender: UIBarButtonItem) {
+        viewLeadingContraint.toggleLeadingContraint()
+    }
+    
+    
 }
 
+extension NSLayoutConstraint{
+    func toggleLeadingContraint(){
+        self.constant = self.constant != 0 ? 0 : CGFloat(integerLiteral: Int(self.identifier!)!)
+    }
+    
+}
